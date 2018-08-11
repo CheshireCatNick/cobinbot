9ce905af94f9452a15796347fb2cbe38056a0865
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-11 11:43:54 +0800

    add la bar

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # candy-bot
 Trading bot for COBINHOOD candy machine.
+
+### Install
+npm install
+
+### Usage
+node main.js
+arguments:
+- -s: sell ETH from account 1 and buy from account 2
+- -is: buy ETH from account 1 and sell from account 2
+- -l: play candy machine
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 const config = require('./config');
 const Cobinhood = require('node-cobinhood');
+const RestClient = require('./lib/rest-client');
 
 // must be greater than 0.01
 const SAFE_SPREAD = 0.1;
@@ -23,19 +24,67 @@ function handleErr(err) {
     process.exit(0);
 }
 
-const a1Cobinhood = new Cobinhood({ key: config.a1_api_key, disableWS: true });
-const a2Cobinhood = new Cobinhood({ key: config.a2_api_key, disableWS: true });
+function laBar() {
+    const restClient = new RestClient('api.cobinhood.com', 443);
+    restClient.get('/v1/campaign/slot_machine/tokens?status=available', config.la_bar_api_key)
+    .then((r) => {
+        if (r.result.tokens === null) {
+            console.log('No available ticket.');
+            return;
+        }
+        for (let ticket of r.result.tokens) {
+            console.log(ticket);
+            const header = {
+                'Content-Type': 'application/json',
+                'Content-Length': 2,
+                'authorization': config.la_bar_api_key,
+                'nonce': new Date().valueOf(),
+                'Origin': 'https://cobinhood.com'
+            };
+            restClient.post('/v1/campaign/slot_machine/play', header, '').then((r) => {
+                console.log(r);
+            }, handleErr);
+        }
 
-a1Cobinhood.getOrderbook('ETH-USDT', '1E-2').then((orderBook) => {
-    const price = getSwapPrice(orderBook);
-    console.log('price = ', price);
+    }, handleErr);
+
+}
+
+function swap(isInverted) {
+    const a1Cobinhood = new Cobinhood({ 
+        key: (isInverted) ? config.a2_api_key : config.a1_api_key, 
+        disableWS: true 
+    });
+    const a2Cobinhood = new Cobinhood({ 
+        key: (isInverted) ? config.a1_api_key : config.a2_api_key, 
+        disableWS: true 
+    });
     
-    a1Cobinhood.placeLimitOrder('ETH-USDT', 'ask', '' + price, '' + SWAP_SIZE)
-    .then((result) => {
-        console.log(result.id, result.state);
-        a2Cobinhood.placeLimitOrder('ETH-USDT', 'bid', '' + price, '' + SWAP_SIZE)
+    a1Cobinhood.getOrderbook('ETH-USDT', '1E-2').then((orderBook) => {
+        const price = getSwapPrice(orderBook);
+        console.log('price = ', price);
+        
+        a1Cobinhood.placeLimitOrder('ETH-USDT', 'ask', '' + price, '' + SWAP_SIZE)
         .then((result) => {
             console.log(result.id, result.state);
-        }, handleErr)
+            a2Cobinhood.placeLimitOrder('ETH-USDT', 'bid', '' + price, '' + SWAP_SIZE)
+            .then((result) => {
+                console.log(result.id, result.state);
+            }, handleErr)
+        }, handleErr);
     }, handleErr);
-}, handleErr);
+}
+
+process.argv.forEach(args => {
+    if (args === '-s') {
+        swap(false);
+    }
+    else if (args === '-is') {
+        swap(true);
+    }
+    else if (args === '-l') {
+        laBar();
+    }
+});
+
+
