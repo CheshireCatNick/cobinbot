9e236cb978f4ae0e17ca71353e816a3efc0e81a9
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-18 01:55:53 +0800

    finish class asset swapper

--- affair/asset-swapper.js
+++ affair/asset-swapper.js
@@ -1,48 +1,53 @@
 'use strict';
 
 const Cobinhood = require('node-cobinhood');
-// must be greater than 0.01
-const SAFE_SPREAD = 0.1;
-const SWAP_SIZE = 0.06;
+const Affair = require('./affair');
 
-function handleErr(err) {
-    console.log(err);
-    process.exit(0);
-}
+class AssetSwapper extends Affair {
 
-function getSwapPrice(orderBook) {
-    const highestBid = orderBook.bids[0].price.toNumber();
-    const lowestAsk = orderBook.asks[0].price.toNumber();
-    console.log('highest bid = ', highestBid);
-    console.log('lowest ask = ', lowestAsk);
-    const spread = lowestAsk - highestBid;
-    if (spread < SAFE_SPREAD) {
-        handleErr('Spread size too small.');
+    getSwapPrice(orderBook) {
+        const highestBid = orderBook.bids[0].price.toNumber();
+        const lowestAsk = orderBook.asks[0].price.toNumber();
+        console.log('highest bid = ', highestBid);
+        console.log('lowest ask = ', lowestAsk);
+        const spread = lowestAsk - highestBid;
+        if (spread < AssetSwapper.SAFE_SPREAD) {
+            super.handleErr('Spread size too small.');
+        }
+        return highestBid + spread / 2;
     }
-    return highestBid + spread / 2;
-}
-
-function swap(isInverted) {
-    const a1Cobinhood = new Cobinhood({ 
-        key: (isInverted) ? config.a2_api_key : config.a1_api_key, 
-        disableWS: true 
-    });
-    const a2Cobinhood = new Cobinhood({ 
-        key: (isInverted) ? config.a1_api_key : config.a2_api_key, 
-        disableWS: true 
-    });
     
-    a1Cobinhood.getOrderbook('ETH-USDT', '1E-2').then((orderBook) => {
-        const price = getSwapPrice(orderBook);
-        console.log('price = ', price);
+    swap(key1, key2, isInverted) {
+        const a1Cobinhood = new Cobinhood({ 
+            key: (isInverted) ? key2 : key1, 
+            disableWS: true 
+        });
+        const a2Cobinhood = new Cobinhood({ 
+            key: (isInverted) ? key1 : key2, 
+            disableWS: true 
+        });
         
-        a1Cobinhood.placeLimitOrder('ETH-USDT', 'ask', '' + price, '' + SWAP_SIZE)
-        .then((result) => {
-            console.log(result.id, result.state);
-            a2Cobinhood.placeLimitOrder('ETH-USDT', 'bid', '' + price, '' + SWAP_SIZE)
+        a1Cobinhood.getOrderbook('ETH-USDT', '1E-2').then((orderBook) => {
+            const price = this.getSwapPrice(orderBook);
+            console.log('price = ', price);
+            
+            a1Cobinhood.placeLimitOrder('ETH-USDT', 'ask', '' + price, '' + AssetSwapper.SWAP_SIZE)
             .then((result) => {
                 console.log(result.id, result.state);
-            }, handleErr)
-        }, handleErr);
-    }, handleErr);
+                a2Cobinhood.placeLimitOrder('ETH-USDT', 'bid', '' + price, '' + AssetSwapper.SWAP_SIZE)
+                .then((result) => {
+                    console.log(result.id, result.state);
+                }, super.handleErr)
+            }, super.handleErr);
+        }, super.handleErr);
+    }
+
+    constructor() {
+        super();
+    }
 }
+// must be greater than 0.01
+AssetSwapper.SAFE_SPREAD = 0.1;
+AssetSwapper.SWAP_SIZE = 0.07;
+
+module.exports = AssetSwapper;
