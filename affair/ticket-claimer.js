44212dd0cf06b56eeb225f36c1199fa2a98210fc
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-18 19:27:26 +0800

    finish ticket claimer

--- affair/ticket-claimer.js
+++ affair/ticket-claimer.js
@@ -1,2 +1,43 @@
 'use strict';
 
+const Affair = require('./affair');
+const RestClient = require('../lib/rest-client');
+const restClient = new RestClient('api.cobinhood.com', 443);
+const Debug = require('../lib/debug');
+
+class TicketClaimer extends Affair {
+
+    claim(token) {
+        const header = {
+            authorization: token,
+            nonce: new Date().valueOf(),
+            'Content-Length': 2,
+        };
+        restClient.post('/v1/campaign/events/cob-point/redemption_cob_point_daily',
+            header,
+            ''
+        ).then((result) => {
+            console.log(result);
+            if (!result.success) {
+                Debug.warning([this.TAG, 'Can not claim ticket.']);
+                return;
+            }
+            if (!result.result.redemption.redeemed) {
+                Debug.info([this.TAG, 'Claimed before.']);
+            }
+            else {
+                Debug.success([this.TAG, 'Ticket claimed.']);
+            }
+        }, super.handleErr);
+    }
+    constructor() {
+        super();
+        this.TAG = 'Ticket Claimer';
+    }
+}
+
+module.exports = TicketClaimer;
+
+//const config = require('../config');
+//const tc = new TicketClaimer();
+//tc.claim(config.token);
