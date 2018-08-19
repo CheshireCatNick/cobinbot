6e1e65e7b2a24fddd727b657bdad94def7d44b6e
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-19 18:33:08 +0800

    remove old comment

--- candy-bot.js
+++ candy-bot.js
@@ -1,27 +1,10 @@
 'use strict';
 const config = require('./config');
-const Debug = require('./lib/debug');
 const TokenRefresher = require('./affair/token-refresher');
 const TicketClaimer = require('./affair/ticket-claimer');
 const CandyMachinePlayer = require('./affair/candy-machine-player');
 
 const TAG = 'Candy Bot';
-/*
-process.argv.forEach(args => {
-    if (args === '-s') {
-        swap(false);
-    }
-    else if (args === '-is') {
-        swap(true);
-    }
-    else if (args === '-l1') {
-        laBar(1);
-    }
-    else if (args === '-l2') {
-        laBar(2)
-    }
-});
-*/
 
 const tr = new TokenRefresher(config.token);
 const tc = new TicketClaimer();
