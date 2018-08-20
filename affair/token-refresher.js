8c0fb45bbd1f6572d4ac6c89007fbeb80e0e24a1
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-20 22:10:12 +0800

    token refresher save latest token

--- affair/token-refresher.js
+++ affair/token-refresher.js
@@ -1,12 +1,27 @@
 'use strict';
 
 const RestClient = require('../lib/rest-client');
+const fs = require('fs');
 const Affair = require('./affair');
 const Debug = require('../lib/debug');
 
 const restClient = new RestClient('api.cobinhood.com', 443);
 class TokenRefresher extends Affair {
 
+    saveToken() {
+        config.token = this.token;
+        let configFile = "'use strict';\n\n";
+        configFile += 'module.exports = {\n';
+        for (let key in config) {
+            configFile += `\t${key}: '${config[key]}',\n`;
+        }
+        configFile += '};\n';
+        fs.writeFile('config.js', configFile, (err) => {
+            if (err) {
+                Debug.warning([this.TAG, 'Can not save config file.']);
+            }
+        });
+    }
     constructor(token) {
         super();
         this.TAG = 'Token Refresher';
@@ -26,9 +41,14 @@ class TokenRefresher extends Affair {
                 Debug.success([this.TAG, 'Successfully refresh token.']);
                 //console.log(result.result.account.token);
                 this.token = result.result.account.token;
+                this.saveToken();
             }, super.handleErr);
         }, TokenRefresher.refreshPeriod);
     }
 }
 TokenRefresher.refreshPeriod = 30 * 1000;
 module.exports = TokenRefresher;
+
+const config = require('../config');
+const tr = new TokenRefresher(config.token);
+tr.saveToken();
