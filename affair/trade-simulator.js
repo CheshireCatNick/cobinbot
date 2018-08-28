3a6a6afb303ed80fd240a73655becd4b4bbd6f6e
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-28 21:34:12 +0800

    implement simulate function

--- affair/trade-simulator.js
+++ affair/trade-simulator.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const Cobinhood = require('../exchange/cobinhood');
+const Wallet = require('../exchange/wallet');
 
 class TradeSimulator {
 
     simulateTrade(order) {
+        const isBuy = order.amount > 0;
+        const amount = Math.abs(order.amount);
+        const base = order.pair.split('-')[0];
+        const quote = order.pair.split('-')[1];
+        const quoteAmount = base * amount;
+        // check price
+        const orderBooks = this.cobinhood.orderBooks;
+
+        // execute order
+        if (isBuy) {
+            if (this.wallet.withdraw(quote, quoteAmount)) {
+                this.wallet.deposit(base, amount);
+            }
+        }
+        else {
+            if (this.wallet.withdraw(base, amount)) {
+                this.wallet.deposit(quote, quoteAmount);
+            }
+        }
         console.log(order);
     }
 
@@ -18,8 +38,11 @@ class TradeSimulator {
         }
     }
 
-    constructor(strategy) {
+    constructor(strategy, initBalance) {
         this.strategy = strategy;
+        this.initWallet = new Wallet(initBalance);
+        this.wallet = new Wallet(initBalance);
+        this.wallet.toString();
         this.cobinhood = new Cobinhood(strategy.subscribedPairs);
     }
 }
@@ -28,7 +51,8 @@ module.exports = TradeSimulator;
 
 const BasicStrategy = require('../strategty/basic-strategy');
 const bs = new BasicStrategy();
-const ts = new TradeSimulator(bs);
+const ts = new TradeSimulator(bs, { ETH: 100 });
 setInterval(() => {
     ts.run();
+    ts.wallet.toString();
 }, bs.period * 1000);
