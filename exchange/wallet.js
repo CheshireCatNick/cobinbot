e73abc76d3719717384f365738dbad99dea04c28
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-28 21:33:22 +0800

    wallet data structure

---
+++ exchange/wallet.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class Wallet {
+
+    withdraw(asset, amount) {
+        if (amount < 0) return false;
+        if (this.balance[asset] === undefined) {
+            return false;
+        }
+        else if (this.balance[asset] - amount >= 0) {
+            this.balance[asset] -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    deposit(asset, amount) {
+        if (amount < 0) return;
+        if (this.balance[asset]) {
+            this.balance[asset] += amount;
+        }
+        else {
+            this.balance[asset] = amount;
+        }
+    }
+
+    toString() {
+        console.log('Your wallet:');
+        for (let asset in this.balance) {
+            console.log(`${asset}: ${this.balance[asset]}`);
+        }
+    }
+
+    constructor(balance) {
+        this.balance = balance;
+    }
+}
+
+module.exports = Wallet;
