fdd2adce01ddd680bff3092ad3c372664c37c6d6
CheshireCatNick <CheshireCatNick@gmail.com>
2018-08-28 21:35:23 +0800

    pair.symbol to pair.name

--- exchange/cobinhood.js
+++ exchange/cobinhood.js
@@ -13,10 +13,10 @@ class Cobinhood {
     setClientCallback() {
         this.client.on('open', () => {
             for (let pair of this.subscribedPairs) {
-                this.client.subscribeOrderbook(pair.symbol, pair.precision, (msg) => {
-                    this.orderBooks[pair.symbol].update(msg);
+                this.client.subscribeOrderbook(pair.name, pair.precision, (msg) => {
+                    this.orderBooks[pair.name].update(msg);
                 }).then(() => {
-                    Debug.success([TAG, `Successfully subscribed to ${pair.symbol}`]);
+                    Debug.success([TAG, `Successfully subscribed to ${pair.name}`]);
                 }).catch((err) => {
                     Debug.warning([TAG, err]);
                     process.exit(0);
@@ -29,7 +29,7 @@ class Cobinhood {
         this.subscribedPairs = subscribedPairs;
         this.orderBooks = {};
         for (let pair of subscribedPairs) {
-            this.orderBooks[pair.symbol] = new OrderBook();
+            this.orderBooks[pair.name] = new OrderBook();
         }
         this.client = new CobinhoodClient({
             'key': config.a1_api_key 
