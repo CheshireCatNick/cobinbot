ee1f303e9d56317785fabfb752a696b98f06e9d5
Kordan Ou <kordan@hitripod.com>
2018-08-31 12:25:32 +0800

    add COB point claimer

--- bot.js
+++ bot.js
@@ -1,18 +1,22 @@
 'use strict';
 const TokenRefresher = require('./affair/token-refresher');
 const TicketClaimer = require('./affair/ticket-claimer');
+const COBPointClaimer = require('./affair/cobpoint-claimer.js');
 const CandyMachinePlayer = require('./affair/candy-machine-player');
 
 const TAG = 'Bot';
 
 const tr = new TokenRefresher();
 const tc = new TicketClaimer();
+const cc = new COBPointClaimer();
 const cmp = new CandyMachinePlayer();
 
 tc.claim(tr.token);
+cc.claim(tr.token);
 cmp.play(tr.token);
 // claim ticket and play candy machine every 24h
 setInterval(() => {
     tc.claim(tr.token);
+    cc.claim(tr.token);
     cmp.play(tr.token);
 }, 24 * 60 * 60 * 1000);
