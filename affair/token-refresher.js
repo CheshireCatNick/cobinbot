df1f5a92714355aa746ac8ec01d5168e7004b697
CheshireCatNick <CheshireCatNick@gmail.com>
2018-09-11 08:49:33 +0800

    refresh token by expiration time in jwt

--- affair/token-refresher.js
+++ affair/token-refresher.js
@@ -4,11 +4,12 @@ const RestClient = require('../lib/rest-client');
 const fs = require('fs');
 const Affair = require('./affair');
 const Debug = require('../lib/debug');
+const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 const restClient = new RestClient('api.cobinhood.com', 443);
 class TokenRefresher extends Affair {
-
+    
     saveToken() {
         config.token = this.token;
         let configFile = "'use strict';\n\n";
@@ -23,31 +24,45 @@ class TokenRefresher extends Affair {
             }
         });
     }
+
+    getExpireTime() {
+        const decodedToken = jwt.decode(this.token);
+        const exp = decodedToken.exp;
+        const now = new Date().valueOf();
+        return exp * 1000 - now - TokenRefresher.earlierTime;
+    }
+
+    refresh() {
+        restClient.post('/v1/account/refresh_token', 
+            { 
+                authorization: this.token,
+                nonce: new Date().valueOf()
+            }, 
+            {}
+        ).then((result) => {
+            if (!result.success) {
+                Debug.warning([this.TAG, 'Can not get refresh token. Retrying...']);
+                this.refresh();
+            }
+            Debug.success([this.TAG, 'Successfully refresh token.']);
+            //console.log(result.result.account.token);
+            this.token = result.result.account.token;
+            this.saveToken();
+            setTimeout(() => {
+                this.refresh();
+            }, this.getExpireTime());
+        }, super.handleErr);
+    }
+
     constructor() {
         super();
         this.TAG = 'Token Refresher';
         this.token = config.token;
-        setInterval(() => {
-            restClient.post('/v1/account/refresh_token', 
-                { 
-                    authorization: this.token,
-                    nonce: new Date().valueOf()
-                }, 
-                {}
-            ).then((result) => {
-                if (!result.success) {
-                    Debug.warning([this.TAG, 'Can not get refresh token.']);
-                    return;
-                }
-                Debug.success([this.TAG, 'Successfully refresh token.']);
-                //console.log(result.result.account.token);
-                this.token = result.result.account.token;
-                this.saveToken();
-            }, super.handleErr);
-        }, TokenRefresher.refreshPeriod);
+        this.refresh();
     }
 }
-TokenRefresher.refreshPeriod = 55 * 60 * 1000;
+// refresh token 5 mins earlier than expiration time
+TokenRefresher.earlierTime = 5 * 60 * 1000;
 module.exports = TokenRefresher;
 
 //const tr = new TokenRefresher();
